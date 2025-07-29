1415ec1e13f0f7c2d98ce71e8cb77a9df42cd03f
ringolo <to_be_determined>
2025-07-29 07:37:42 +0200

    added previous paths and CLASSES to __init__

--- src/streamlit/assets/__init__.py
+++ src/streamlit/assets/__init__.py
@@ -1,12 +1,46 @@
+import os
+import sys
+import pandas as pd
 from pathlib import Path
 from types import SimpleNamespace
 
 # project root path
-root = Path(__file__).resolve().parent.parent.parent.parent
+project_root = Path(__file__).resolve().parent.parent.parent.parent
+if not project_root in [Path(p).resolve() for p in sys.path]:
+    sys.path.append(str(project_root))
+
+import src
 
 PATHS = SimpleNamespace(
-    streamlit =         root / "src" / "streamlit",
-    streamlit_images =  root / "src" / "streamlit" / "assets" / "images",
-    stylesheet =        root / "src" / "streamlit" / "assets" / "style.css",
+    **src.PATHS.__dict__,
+    streamlit =         project_root / "src" / "streamlit",
+    streamlit_images =  project_root / "src" / "streamlit" / "assets" / "images",
+    stylesheet =        project_root / "src" / "streamlit" / "assets" / "style.css",
 )
+
+
+try:
+    df = pd.read_parquet(
+        os.path.join(PATHS.metadata, "df_labels_mapping.parquet"))
+    CLASSES = dict(zip(df.index, df.values[:,0]))
+except:
+    print("unable to load CLASSES. Imputing default value")
+    CLASSES = {
+        0: 'letter',
+        1: 'form',
+        2: 'email',
+        3: 'handwritten',
+        4: 'advertisement',
+        5: 'scientific report',
+        6: 'scientific publication',
+        7: 'specification',
+        8: 'file folder',
+        9: 'news article',
+        10: 'budget',
+        11: 'invoice',
+        12: 'presentation',
+        13: 'questionnaire',
+        14: 'resume',
+        15: 'memo'
+        }
 
