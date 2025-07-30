4f34ababf849a430450fae952027904840e10fc3
ringolo <to_be_determined>
2025-07-30 07:21:49 +0200

    added some paths and paths creation on init

--- src/__init__.py
+++ src/__init__.py
@@ -3,16 +3,29 @@ from pathlib import Path
 from types import SimpleNamespace
 
 # project root is determined as being parent directory of src
-root = Path(__file__).resolve().parents[1]
+project_root = Path(__file__).resolve().parents[1]
 
 PATHS = SimpleNamespace(
-    project = root,
-    data = os.path.join(root, "data"),
-    raw_data = os.path.join(root, "data", "raw"),
-    raw_images = os.path.join(root, "data", "raw", "RVL-CDIP", "images"),
-    converted_images = os.path.join(root, "data", "converted", "images"),
-    metadata = os.path.join(root, "data", "metadata"),
-    processed_data = os.path.join(root, "data", "processed"),
-    models = os.path.join(root, "models"),
-    pipelines = os.path.join(root, "pipelines"),
+    project =           project_root,
+    data =              project_root / "data",
+    raw_data =          project_root / "data" / "raw",
+    rvl_cdip =          project_root / "data" / "raw" / "RVL-CDIP",
+    rvl_cdip_images =   project_root / "data" / "raw" / "RVL-CDIP" / "images",
+    iit_cdip_images =   project_root / "data" / "raw" / "IIT-CDIP" / "images",
+    iit_cdip_xmls =     project_root / "data" / "raw" / "IIT-CDIP" / "xmls",
+    raw_images =        project_root / "data" / "raw" / "RVL-CDIP" / "images", # same as rvl_cdip
+    labels =            project_root / "data" / "raw" / "labels",
+    metadata =          project_root / "data" / "metadata",
+    samples =           project_root / "data" / "metadata" / "samples",
+    converted_images =  project_root / "data" / "converted" / "images",
+    processed_data =    project_root / "data" / "processed",
+    models =            project_root / "models",
+    pipelines =         project_root / "pipelines",
 )
+
+
+def create_all_dirs(paths: SimpleNamespace) -> None:
+    for path in paths.__dict__.values():
+        os.makedirs(path, exist_ok=True)
+# Allows to avoid any error linked to missing path
+create_all_dirs(PATHS)
