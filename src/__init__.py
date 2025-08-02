604c0d30e305a36336c299f0f8acea5cdf3ffed5
ringolo <to_be_determined>
2025-08-02 22:38:59 +0200

    ajoute LABELS a src

--- src/__init__.py
+++ src/__init__.py
@@ -29,3 +29,22 @@ def create_all_dirs(paths: SimpleNamespace) -> None:
         os.makedirs(path, exist_ok=True)
 # Allows to avoid any error linked to missing path
 create_all_dirs(PATHS)
+
+LABELS = {
+    0: 'letter',
+    1: 'form',
+    2: 'email',
+    3: 'handwritten',
+    4: 'advertisement',
+    5: 'scientific report',
+    6: 'scientific publication',
+    7: 'specification',
+    8: 'file folder',
+    9: 'news article',
+    10: 'budget',
+    11: 'invoice',
+    12: 'presentation',
+    13: 'questionnaire',
+    14: 'resume',
+    15: 'memo'
+    }
