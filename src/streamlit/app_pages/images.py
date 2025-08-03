9692540b3bccc2fb8178d7eb83f03eb5d42eba9a
Camille-Les <lescanne.camille@gmail.com>
2025-08-03 09:08:24 +0200

    notebooks de DL images

--- src/streamlit/app_pages/images.py
+++ src/streamlit/app_pages/images.py
@@ -73,7 +73,7 @@ Voici le processus en image :
     st.subheader("Résultats du machine learning sur notre échantillon de 10 000 images")
     
     with st.expander("Resultats du SGD Classifier"):
-        st.markdown(f"""Avec les meilleurs paramètres (Grid Search): F1 Score 0,17""")
+        st.markdown(f"""Avec les meilleurs paramètres (Grid Search): Accuracy 0,17""")
         col1, col2 = st.columns([0.4, 0.6])
         with col1:
             st.image(os.path.join(PATHS.streamlit, "assets", "images","sgd_spider.png"), caption="Représentation de l'accuracy du XGBoost")
@@ -81,7 +81,7 @@ Voici le processus en image :
             st.image(os.path.join(PATHS.streamlit, "assets", "images","sgd_cm.png"), caption="Matrice de confusion pour le XGBoost")
 
     with st.expander("Resultats du XGBoost Classifier"):
-        st.markdown(f"""Avec les meilleurs paramètres (Grid Search): F1 Score 0,37""")
+        st.markdown(f"""Avec les meilleurs paramètres (Grid Search): Accuracy 0,37""")
         col1, col2 = st.columns([0.4, 0.6])
         with col1:
             st.image(os.path.join(PATHS.streamlit, "assets", "images","xgb_spider.png"), caption="Représentation de l'accuracy du XGBoost")
@@ -89,7 +89,7 @@ Voici le processus en image :
             st.image(os.path.join(PATHS.streamlit, "assets", "images","xgb_cm.png"), caption="Matrice de confusion pour le XGBoost")
 
     with st.expander("Resultats du LGBM Classifier"):
-        st.markdown(f"""Avec les meilleurs paramètres (Grid Search): F1 Score 0,42""")
+        st.markdown(f"""Avec les meilleurs paramètres (Grid Search): Accuracy 0,42""")
         col1, col2 = st.columns([0.4, 0.6])
         with col1:
             st.image(os.path.join(PATHS.streamlit, "assets", "images","lgbm_spider.png"), caption="Représentation de l'accuracy du LGBM")
@@ -102,7 +102,7 @@ Voici le processus en image :
     st.markdown(f"""Pour le LGBM, avec les meilleurs paramètres, cette fois :
 - entrainé sur 320 000 images 
 - testé sur 4 000 images\n
-F1 Score 0,55""")
+Accuracy 0,55""")
     col1, col2 = st.columns([0.4, 0.6])
     with col1:
         st.image(os.path.join(PATHS.streamlit, "assets", "images","lgbm_spider_all_images.png"), caption="Représentation de l'accuracy du LGBM")
@@ -117,7 +117,7 @@ F1 Score 0,55""")
     data2 = pd.DataFrame({
         'Model': ['LGBMClassifier', 'XGBClassifier', 'SGDCLassifier'],
         'Durée de Bayes Search (min)': [72, 323, 3],
-        'F1 Score sur le set de valisation': [0.42, 0.37, 0.17]
+        'Accuracy sur le set de valisation': [0.42, 0.37, 0.17]
     })
     st.table(data2)
     
@@ -146,7 +146,7 @@ Aussi, aucune augmentation n'a été effectuée:
     with st.expander("ResNet50 - 10 couches de fine-tuning"):
         st.markdown(f"""Le premier ResNet avec des résultats intéressants a subi un peu de fine-tuning: les 10 dernières couches de convolution ont été dégelées\n
 Pour limiter le temps d'execution, l'ensemble a été fait sur un échantillon de 40 000 images (1/10 de l'ensemble des images)
-Le F1-score final obtenu sur le set de validation de 4000 images: 0,74
+L'accuracy finale obtenue sur le set de validation de 4000 images: 0,74
     """)
         st.image(os.path.join(PATHS.streamlit, "assets", "images","ResNet_1_history.png"), caption="Evaluation de la fonction de perte et de l'accuracy au fil des epoch")
         st.markdown(f"""50 epochs avaient été programmés mais l'apprentissage s'est arrêté à 17 pour absence d'amélioration""")
@@ -170,7 +170,7 @@ Les “history” des 3 entrainements ont été concaténées et voici leur rés
         
         st.image(os.path.join(PATHS.streamlit, "assets", "images","ResNet_2_history.png"), caption="Evaluation de la fonction de perte et de l'accuracy au fil des epoch")
         st.markdown(f"""Là encore, pour limiter le temps d'execution, l'ensemble a été fait sur un échantillon de 40 000 images (1/10 de l'ensemble des images)
-Le F1-score final obtenu sur le set de validation de 4000 images: 0,78
+L'accuracy finale obtenue sur le set de validation de 4000 images: 0,78
     """)
         col1, col2 = st.columns([0.4, 0.6])
         with col1:
@@ -186,7 +186,7 @@ Le F1-score final obtenu sur le set de validation de 4000 images: 0,78
     with st.expander("VGG16 - 4 couches de fine-tuning"):
         st.markdown(f"""Le premier VGG16 avec des résultats intéressants a subi un peu de fine-tuning: les 4 dernières couches de convolution ont été dégelées\n
 Pour limiter le temps d'execution, l'ensemble a été fait sur un échantillon de 40 000 images (1/10 de l'ensemble des images)
-Le F1-score final obtenu sur le set de validation de 4000 images: 0,79
+L'accuracy finale obtenue sur le set de validation de 4000 images: 0,79
     """)
         st.image(os.path.join(PATHS.streamlit, "assets", "images","VGG16_1_history.png"), caption="Evaluation de la fonction de perte et de l'accuracy au fil des epoch")
         st.markdown(f"""50 epochs avaient été programmés mais l'apprentissage s'est arrêté à 25 pour absence d'amélioration""")
@@ -199,9 +199,9 @@ Le F1-score final obtenu sur le set de validation de 4000 images: 0,79
             st.image(os.path.join(PATHS.streamlit, "assets", "images","vgg16_1_cm.png"), caption="Matrice de confusion - VGG16 - 4 couches")
 
     with st.expander("VGG16 -  Degel progressif automatique des couches"):
-        st.markdown(f"""Cette fois, un nouveau callback a été ajouté, qui dégèle 5 nouvelles couches à fine-tuner quand la fonction de perte stagne pendant 2 epochs (et on part d'un VGG16 avec 5 couches dégelées dès le départ)\n
+        st.markdown(f"""Cette fois, un nouveau callback a été ajouté, qui dégèle 5 nouvelles couches à fine-tuner quand la fonction de perte ,n'est pas meilleure que la meilleure précédemment enregistrée pendant 5 epochs (et on part d'un VGG16 avec 4 couches dégelées dès le départ)\n
 Pour limiter le temps d'execution, l'ensemble a été fait sur un échantillon de 40 000 images (1/10 de l'ensemble des images)
-Le F1-score final obtenu sur le set de validation de 4000 images: 
+L'accuracy finale obtenue sur le set de validation de 4000 images: 0.61
     """)
         st.image(os.path.join(PATHS.streamlit, "assets", "images","VGG16_2_history.png"), caption="Evaluation de la fonction de perte et de l'accuracy au fil des epoch")
         st.markdown(f"""50 epochs avaient été programmés mais l'apprentissage s'est arrêté à """)
@@ -219,7 +219,7 @@ Le F1-score final obtenu sur le set de validation de 4000 images:
     'Model': ['ResNet50 - 10 couches', 'ResNet50 - Degel progressif manuel', 'VGG16 - 4 couches', 'VGG16 - Degel progressif automatique'],
     "Epochs avant arrêt": ["18/50", "Etape 1: 9/10 <br>Etape 2: 6/10<br>Etape 3: 6/10", "25/50", "29/50"],
     "Durée d'entrainement": ["2h45", "Etape 1: 1h25 <br>Etape 2: 1h11<br>Etape 3: 1h20", "9h", "18h"],
-    'F1 Score sur le set de validation': [0.74, 0.78, 0.79, 0.81]
+    'Accuracy sur le set de validation': [0.74, 0.78, 0.79, 0.81]
     })
 
     # Création du tableau HTML
